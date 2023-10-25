a19cc5099db538eee0c50977894eaef584cfe393
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-10-25 17:18:31 +0300

    update пока так

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Button } from 'components/Button/Button';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Searchbar } from 'components/Searchbar/Searchbar';
+import React from 'react';
+import { fetchPhotos } from 'Services/api';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Searchbar />
+        <ImageGallery />
+        <Button />
+      </div>
+    );
+  }
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import React from 'react';
+
+export const ImageGallery = () => {
+  return (
+    <div>
+      <ul class="gallery">
+        <ImageGalleryItem />
+      </ul>
+    </div>
+  );
+};
