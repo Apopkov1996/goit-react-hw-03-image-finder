d9cbdbbdc38e60b8de4555a8cf8d68db775fe8c6
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-10-27 15:47:10 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <>
+  <React.StrictMode>
     <App />
     <ToastContainer autoClose={2500} position="top-right" />
-  </>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
